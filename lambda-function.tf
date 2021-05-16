d208fa2452c29cfa99fca4a45063ad082e960f88
Pavel Demichev <pasha.demichev@gmail.com>
2021-05-16 22:13:51 +1200

    rename lambda tf file

---
+++ lambda-function.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  profile = "default"
+  region  = var.region
+}
+
+provider "archive" {}
+
+resource "null_resource" "install_python_dependencies" {
+  provisioner "local-exec" {
+    #command = "set -e; mkdir -p ${path.module}/tmp/package-dependencies; rm -rf ${path.module}/tmp/package-dependencies/*; pip install -r ${path.module}/python/requirements.txt -t ${path.module}/tmp/package-dependencies; cp -r ${path.module}/python/*.py ${path.module}/tmp/package-distribute/; chmod -R 755 ${path.module}/tmp/package-dependencies"
+    command = "mkdir -force ${path.module}\\tmp\\package-dependencies; rm -recurse -force ${path.module}\\tmp\\package-dependencies\\*; pip install -r ${path.module}/python/requirements.txt -t ${path.module}/tmp/package-dependencies; cp ${path.module}\\python\\*.py -Destination ${path.module}\\tmp\\package-dependencies\\ -Recurse"
+    interpreter = ["powershell"]
+  }
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+}
+
+resource "aws_lambda_function" "ami_cleaner_lambda" {
+  runtime          = "python3.7"
+  function_name    = "ami_cleaner"
+  description      = "Lambda function for cleanering AMI and deleting snapshots"
+  timeout          = 5
+  handler          = "ami_cleaner.lambda_handler"
+  filename         = data.archive_file.zip.output_path
+  source_code_hash = data.archive_file.zip.output_base64sha256
+  role             = aws_iam_role.ami_cleaner_lambda_role.arn
+
+  tags = {
+    Name = "ami cleaner"
+  }
+}
